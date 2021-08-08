9bcfb54ed2b9d9e2dbb10f70f8eb6d10fe3979e8
Naomi Burkhart <naomilburkhart@gmail.com>
2021-08-08 16:39:04 -0700

    update Yatzy class methods to use common naming scheme (lower_case); fix PEP 8 linting errors

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,7 +1,7 @@
 class Yatzy:
 
     def __init__(self, d1, d2, d3, d4, d5):
-        self.dice = [0]*5
+        self.dice = [0] * 5
         self.dice[0] = d1
         self.dice[1] = d2
         self.dice[2] = d3
@@ -60,10 +60,9 @@ class Yatzy:
     def score_pair(self):
         counts = self._get_die_counts()
 
-        at = 0
         for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
+            if counts[6 - at - 1] == 2:
+                return (6 - at) * 2
         return 0
 
     def two_pair(self):
@@ -72,11 +71,11 @@ class Yatzy:
         n = 0
         score = 0
         for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
+            if counts[6 - i - 1] >= 2:
+                n = n + 1
+                score += (6 - i)
+
+        if n == 2:
             return score * 2
         else:
             return 0
@@ -85,43 +84,42 @@ class Yatzy:
         counts = self._get_die_counts()
 
         for i in range(6):
-            if (counts[i] >= 4):
-                return (i+1) * 4
+            if counts[i] >= 4:
+                return (i + 1) * 4
         return 0
 
     def three_of_a_kind(self):
         counts = self._get_die_counts()
 
         for i in range(6):
-            if (counts[i] >= 3):
-                return (i+1) * 3
+            if counts[i] >= 3:
+                return (i + 1) * 3
         return 0
 
-    def smallStraight(self):
+    def small_straight(self):
         counts = self._get_die_counts()
 
         if (counts[1] == 1 and
-            counts[2] == 1 and
-            counts[3] == 1 and
-            counts[4] == 1):
+                counts[2] == 1 and
+                counts[3] == 1 and
+                counts[4] == 1):
             if counts[0] == 1 or counts[5] == 1:
                 return 30
         return 0
 
-    def largeStraight(self):
+    def large_straight(self):
         counts = self._get_die_counts()
 
         if (counts[1] == 1 and
-            counts[2] == 1 and
-            counts[3] == 1 and
-            counts[4] == 1
-            and counts[5] == 1):
+                counts[2] == 1 and
+                counts[3] == 1 and
+                counts[4] == 1
+                and counts[5] == 1):
             return 40
         return 0
 
-    def fullHouse(self):
+    def full_house(self):
         _2 = False
-        i = 0
         _2_at = 0
         _3 = False
         _3_at = 0
@@ -129,16 +127,16 @@ class Yatzy:
         counts = self._get_die_counts()
 
         for i in range(6):
-            if (counts[i] == 2):
+            if counts[i] == 2:
                 _2 = True
-                _2_at = i+1
+                _2_at = i + 1
 
         for i in range(6):
-            if (counts[i] == 3):
+            if counts[i] == 3:
                 _3 = True
-                _3_at = i+1
+                _3_at = i + 1
 
-        if (_2 and _3):
+        if _2 and _3:
             return 25
         else:
             return 0
